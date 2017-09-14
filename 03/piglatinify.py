b96ea718b200de04b5ff4aa17479c72ff79f18bb
Angela <Angela.Lim57@myhunter.cuny.edu>
2017-09-14 19:21:24 -0400

    first commit

---
+++ 03/piglatinify.py
@@ -0,0 +1,7 @@
+def piglatin(word):
+    vowel = ('a', 'e', 'i', 'o', 'u')
+    first_letter = word[0]
+    if first_letter in vowel:
+        return word + 'ay'
+    else:
+        return word[1:] + word[0] + 'ay'
